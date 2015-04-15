e792fd3e79f94cf35d78cdd2da310db37c0adddf
Margal07 <margot.fargier@gmail.com>
2015-04-15 14:22:14 +0200

    propre avec map guillou et debut de code pour villageois

---
+++ Mif39/Assets/Medieval/Collision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		for(int i=0; i < transform.childCount ; i++)
+		{
+			transform.GetChild(i).gameObject.AddComponent<MeshCollider>();
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Mif39/Assets/Medieval/ComportementVillageois.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComportementVillageois : MonoBehaviour {
+
+	// Pour l'instant, on suppose que le villageois (personnage) est présent dans la map, on va y attacher ce script de comportement
+	// Use this for initialization
+	void Start () 
+	{
+		transform.gameObject.AddComponent<MeshCollider>();
+		//transform.position = Vector3(44, 32.507, 92);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
